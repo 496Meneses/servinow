a4066f01065c1d645df0b98c7cb176b40f5682a3
lino <linomu@unicauca.edu.co>
2021-04-13 21:50:07 -0500

    Se creó el componente para realizar todos los filtros de búsqueda

--- src/pages/Grupo/Solicitud/Solicitante/index.js
+++ src/pages/Grupo/Solicitud/Solicitante/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import { OfertListContainer } from "../david/containers/OfertListContainer";
 
-function index() {
-    return (
-        <div>
-            <h1>Here</h1>
-        </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function CenteredGrid() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={8}>
+          <Paper className={classes.paper}>cuadro de Busqueda</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Select</Paper>
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          
+              <OfertListContainer></OfertListContainer>
+          
+        </Grid>
+        
+      </Grid>
+    </div>
+  );
 }
-
-export default index
